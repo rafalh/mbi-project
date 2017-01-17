968018a03dd2dcb1213e8789642700cb33d30501
PatrykLucka <patryk.lucka@gmail.com>
2017-01-17 19:48:17 +0100

    Update RegionResults.scala

--- src/main/scala/pl/edu/pw/elka/mbi/genomecoverage/RegionResults.scala
+++ src/main/scala/pl/edu/pw/elka/mbi/genomecoverage/RegionResults.scala
@@ -3,6 +3,6 @@ package pl.edu.pw.elka.mbi.genomecoverage
 /**
   * Created by rafalh on 09.01.17.
   */
-class RegionResults(val min: Integer, val max: Integer, val mean: Double, val stdDev: Double) {
+class RegionResults(val length: Integer, val sum: Integer, val min: Integer, val max: Integer, val mean: Double, val stdDev: Double) {
 
 }
