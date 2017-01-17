f7414a2c968d1223397244ed4211a95a3318f0de
PatrykLucka <patryk.lucka@gmail.com>
2017-01-17 19:47:34 +0100

    Update App.scala

--- src/main/scala/pl/edu/pw/elka/mbi/genomecoverage/App.scala
+++ src/main/scala/pl/edu/pw/elka/mbi/genomecoverage/App.scala
@@ -15,10 +15,6 @@ object App {
     }
   }
 
-  def buildResult(depths: RDD[Int]): RegionResults = {
-    new RegionResults(min = depths.min, max = depths.max, mean = depths.mean, stdDev = depths.stdev)
-  }
-
   def main(args: Array[String]) {
     val bamFile = "local-data/HG00096.chrom20.ILLUMINA.bwa.GBR.exome.20120522.bam"
 
@@ -33,50 +29,71 @@ object App {
       .filter(rec => rec.getMappingQuality > 15)
       .filter(rec => !rec.getReadUnmappedFlag && !rec.getNotPrimaryAlignmentFlag)
 
-//    records
-//      .take(5)
-//      .foreach(rec => {
-//        println(rec)
-//        println(s"${rec.getReferenceName}: ${rec.getAlignmentStart} - ${rec.getAlignmentEnd} - ${rec.getReadString}")
-//      })
-
-    val bedFile = sc.textFile("local-data/20130108.exome.targets.bed")
+    /*records
+      .take(5)
+      .foreach(rec => {
+        println(rec)
+        println(s"${rec.getReferenceName}: ${rec.getAlignmentStart} - ${rec.getAlignmentEnd} - ${rec.getReadString}")
+      })*/
+    val bedFile = sc.textFile("local-data/20130108.exome.targets-edited-one.bed")
     val regions = bedFile
       .map(line => line.split("\t"))
       .map(fields => new ChromoRegion(parseChromo(fields(0)), Integer.valueOf(fields(1)), Integer.valueOf(fields(2))))
+    println("bam count - " + records.count());
+    println("bed count - " + regions.count());
 
     regions
       .cartesian(records)
       .filter(pair => { // find overlapping pairs (region and read align)
-        val reg = pair._1
+      val reg = pair._1
         val rec = pair._2
-        rec.getAlignmentStart <= reg.end && rec.getAlignmentEnd <= reg.start
+        rec.getAlignmentStart <= reg.end && rec.getAlignmentEnd >= reg.start
+      })
+      .foreach(pair => {
+        println(pair._1 + ": " + pair._2);
+        println(s"${pair._2.getReferenceName}: ${pair._2.getAlignmentStart} - ${pair._2.getAlignmentEnd} - ${pair._2.getReadString}")
+      });
+
+    regions
+      .cartesian(records)
+      .filter(pair => { // find overlapping pairs (region and read align)
+      val reg = pair._1
+        val rec = pair._2
+        rec.getAlignmentStart <= reg.end && rec.getAlignmentEnd >= reg.start
       })
       .map(pair => { // map to array of region length which value is 1 if position is contained in record, 0 otherwise
         val reg = pair._1
         val rec = pair._2
-        val temp = Array(0, reg.size) // optimization?
+        val temp = Array.fill[Int](reg.size+1)(0) // optimization?
         for (i <- 0 to reg.size)
-          if (reg.start + i >= rec.getAlignmentStart && reg.start + i <= rec.getAlignmentEnd)
+          if (reg.start + i >= rec.getAlignmentStart && reg.start + i <= rec.getAlignmentEnd) {
             temp(i) = 1
+          }
         (reg, temp)
       })
-      .reduceByKey((x, y) => x.zip(y).map(p => p._1 + p._2)) // sum arrays for the same region
+      .reduceByKey((x, y) => x.zip(y).map(p => p._1 + p._2))
       .map(pair => { // calculate interesting results
         val depths = pair._2
         val sum = depths.sum
         val mean = sum / depths.length
         (pair._1, new RegionResults(
-          // Can we do it better? RDD has needed methods...
-          min = depths.min,
-          max = depths.max,
-          mean = mean,
-          stdDev = Math.sqrt(depths.map( _ - mean).map(t => t*t).sum / depths.length)))
+            // Can we do it better? RDD has needed methods...
+            length = depths.length,
+            sum = sum,
+            min = depths.min,
+            max = depths.max,
+            mean = mean,
+            stdDev = Math.sqrt(depths.map( _ - mean).map(t => t*t).sum / depths.length)))
       })
-      .take(5)
       .foreach(pair => {
-        println(pair._1 + ": " + pair._2)
-      })
+        println(pair._1 + ": " + pair._2);
+        println("length: " + pair._2.length);
+        println("sum: " + pair._2.sum);
+        println("min: " + pair._2.min);
+        println("max: " + pair._2.max);
+        println("mean: " + pair._2.mean);
+        println("stdDev: " + pair._2.stdDev);
+      });
     sc.stop()
   }
 }
